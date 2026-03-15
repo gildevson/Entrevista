af41b64d456e5a11c150beeaeca43ac888d62cf6
gildevson <gildevson@gmail.com>
2026-03-15 18:55:53 -0300

    first commit

---
+++ Entrevista/Controllers/UsuariosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Entrevista.Model;
+
+namespace Entrevista.Controllers {
+
+    [ApiController]
+    [Route("api/controller")]
+    public class UsuarioController : ControllerBase {
+        private static List<Usuario> usuarios = new List<Usuario>();
+
+        [HttpGet("todosusuarios")]
+        public IActionResult GetUsuarios() {
+            return Ok(usuarios);
+
+        }
+        [HttpPost("CriarUsuario")]
+        public IActionResult CriarUsuario(Usuario usuario) {
+                usuario.id = Guid.NewGuid();
+                usuarios.Add(usuario);
+
+                return Ok(usuario);
+        }
+    }
+
+}
---
+++ Entrevista/Model/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace Entrevista.Model {
+    public class Usuario {
+        public Guid id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime Data { get; set; } = DateTime.Now; // data hora atual
+    }
+}
